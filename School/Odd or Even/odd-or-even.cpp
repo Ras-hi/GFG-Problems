9cc836b857b1da2f9f52b2ba3d91c785adb0031b
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-02-26 19:54:43 +0530

    Added Solution - GfG to GitHub

---
+++ School/Odd or Even/odd-or-even.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string oddEven(int N){
+        // code here 
+        //check if the last bit is set or not if it is set then it is odd else it is even .
+        if (N & 1) {
+            return "odd";
+        } 
+        else {
+            return "even";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.oddEven(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends

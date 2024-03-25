37efb36252c7dcd0b98516dbb0a6d897461c4804
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-03-25 22:14:08 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -8,37 +8,23 @@ class Solution {
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-        // boolean list to mark all the vertices as not visited.
-        vector<bool> vis(V, false);
-        int s = 0;
-
-        // initially we mark first vertex as visited(true).
-        vis[s] = true;
-
-        vector<int> res;
-
-        // creating a queue for BFS and pushing first vertex in queue.
-        queue<int> q;
-        q.push(s);
-
-        while (!q.empty()) {
-            // adding front element in output list and popping it from queue.
-            int t = q.front();
-            res.push_back(t);
+        int vis[V]={0};
+        vis[0]=1;
+        queue<int>q;
+        q.push(0);
+        vector<int>bfs;
+        while(!q.empty()){
+            int node=q.front();
             q.pop();
-
-            // traversing over all the connected components of front element.
-            for (int v : adj[t]) {
-                // if they aren't visited, we mark them visited and add to
-                // queue.
-                if (!vis[v]) {
-                    vis[v] = true;
-                    q.push(v);
+            bfs.push_back(node);
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
                 }
             }
         }
-        // returning the output list.
-        return res;
+        return bfs;
     }
 };
 

a10523868ebe38ea61d6cdf239fe682dfc198add
Rashi Gupta <127600601+Ras-hi@users.noreply.github.com>
2024-07-10 21:12:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Shortest un-ordered subarray/shortest-unordered-subarray.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    int shortestUnorderedSubarray(int arr[], int n)
+    {
+        if(n<3){
+            return 0;
+        }
+        else{
+            for(int i=0;i<n-2;i++){
+                if(arr[i]>arr[i+1] && arr[i+1]<arr[i+2]){
+                    return 3;
+                }
+                else if(arr[i]<arr[i+1]&& arr[i+1]>arr[i+2])
+                    return 3;
+            }
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) 
+            cin >> a[i];
+        Solution obj;
+        cout << obj.shortestUnorderedSubarray(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
